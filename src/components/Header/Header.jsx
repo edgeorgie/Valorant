0b4eb7abeefb8ddd1c1c8cbd45e0c784f031a687
Edwin Jorge <ed.jorge1122@gmail.com>
2022-03-19 16:47:50 -0500

    FIX: re-ordered de components in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,41 +1,12 @@
 import React from 'react'
+import { HeaderMenu } from '../HeaderMenu/HeaderMenu'
 import { Logo } from '../Logo/Logo'
-import { Link } from 'react-router-dom'
-import './Styles.scss'
 
 export const Header = () => {
   return (
     <header>
       <Logo />
-      <nav>
-        <ul className='with-indicator'>
-          <li className='Nav-item is-active'>
-            <Link to='/agentes'>
-              Agentes
-            </Link>
-          </li>
-          <li className='Nav-item'>
-            <Link to='/cards'>
-              Cards
-            </Link>
-          </li>
-          <li className='Nav-item'>
-            <Link to='/armas'>
-              Armas
-            </Link>
-          </li>
-          <li className='Nav-item'>
-            <Link to='/titulos'>
-              Títulos
-            </Link>
-          </li>
-          <li className='Nav-item'>
-            <Link to='/modos'>
-              Modos
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <HeaderMenu />
     </header>
   )
 }
