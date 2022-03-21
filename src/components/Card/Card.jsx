1ae724e743a1a3d5ec56a1f48c5e53fdb80b3928
Edwin Jorge <ed.jorge1122@gmail.com>
2022-03-21 18:04:48 -0500

    ADD: loading effect on fetching agents

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { motion } from 'framer-motion'
 import { Model } from '../Models/ModelAgent/Model'
 import { Backdrop } from '../Backdrop/Backdrop'
@@ -8,6 +8,22 @@ export const Card = ({ agente, ability1, ability2, ability3, ability4 }) => {
   const [show, setShow] = useState(false)
   const [heart, setHeart] = useState(false)
   const [selectedId, setSelectedId] = useState(null)
+  const [showCard, setShowCard] = useState(false)
+
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const { isIntersecting } = entries[0]
+        console.log(isIntersecting)
+        if (isIntersecting) {
+          setShowCard(true)
+          observer.disconnect()
+        }
+      })
+    observer.observe(ref.current)
+  }, [ref])
 
   const handleShow = () => {
     setShow(!show)
@@ -23,7 +39,7 @@ export const Card = ({ agente, ability1, ability2, ability3, ability4 }) => {
 
   return (
     <>
-      <motion.div
+      <motion.div ref={ref}
         className='card-container--img'
         style={{
           backgroundImage: `url(${agente.bustPortrait})`,
@@ -31,68 +47,75 @@ export const Card = ({ agente, ability1, ability2, ability3, ability4 }) => {
           backgroundSize: 'cover',
           backgroundRepeat: 'no-repeat'
         }}
+        // animar al entrar en el viewport
+        transition={{ duration: 0.7 }}
+        animate={{ opacity: showCard ? 1 : 0 }}
       >
-        <div className='card-container'>
-          <div
-            className={`box ${
-              show === false
-                ? 'agent-content'
-                : 'agent-content-click-responsive'
-            }`}
-            onClick={handleShow}
-          >
-            <div className='agent-info-container'>
-              <div className='agent-info-principal'>
-                <span className='agent-role'>{agente.role.displayName}</span>
-                <h3 className='agent-name'>{agente.displayName}</h3>
-              </div>
-
-              <div className='agent-info-secondary'>
-                <p className='agent-description'>{agente.description}</p>
-
-                <div className='agent-abilities-container'>
-                  <div className='agent-ability1'>
-                    <img src={ability1.displayIcon} alt='Ability icon' />
-                    <small className='ability-name'>
-                      {ability1.displayName}
-                    </small>
-                  </div>
-                  <div className='agent-ability2'>
-                    <img src={ability2.displayIcon} alt='Ability icon' />
-                    <small className='ability-name'>
-                      {ability2.displayName}
-                    </small>
-                  </div>
-                  <div className='agent-grenade'>
-                    <img src={ability3.displayIcon} alt='Ability icon' />
-                    <small className='ability-name'>
-                      {ability3.displayName}
-                    </small>
-                  </div>
-                  <div className='agent-ultimate'>
-                    <img src={ability4.displayIcon} alt='Ability icon' />
-                    <small className='ability-name'>
-                      {ability4.displayName}
-                    </small>
-                  </div>
+        {
+          showCard && (
+            <div className='card-container'>
+            <div
+              className={`box ${
+                show === false
+                  ? 'agent-content'
+                  : 'agent-content-click-responsive'
+              }`}
+              onClick={handleShow}
+            >
+              <div className='agent-info-container'>
+                <div className='agent-info-principal'>
+                  <span className='agent-role'>{agente.role.displayName}</span>
+                  <h3 className='agent-name'>{agente.displayName}</h3>
                 </div>
 
-                <div className='buttons-container'>
-                  <button className='read-more' onClick={() => readMore()}>
-                    Read more
-                  </button>
-                  <button className='add-favorite' onClick={handleHeart}>
-                    <i
-                      className={`box ${
-                        heart === false ? 'heart' : 'heart heart-click'
-                      }`}
-                    />
-                  </button>
+                <div className='agent-info-secondary'>
+                  <p className='agent-description'>{agente.description}</p>
+
+                  <div className='agent-abilities-container'>
+                    <div className='agent-ability1'>
+                      <img src={ability1.displayIcon} alt='Ability icon' />
+                      <small className='ability-name'>
+                        {ability1.displayName}
+                      </small>
+                    </div>
+                    <div className='agent-ability2'>
+                      <img src={ability2.displayIcon} alt='Ability icon' />
+                      <small className='ability-name'>
+                        {ability2.displayName}
+                      </small>
+                    </div>
+                    <div className='agent-grenade'>
+                      <img src={ability3.displayIcon} alt='Ability icon' />
+                      <small className='ability-name'>
+                        {ability3.displayName}
+                      </small>
+                    </div>
+                    <div className='agent-ultimate'>
+                      <img src={ability4.displayIcon} alt='Ability icon' />
+                      <small className='ability-name'>
+                        {ability4.displayName}
+                      </small>
+                    </div>
+                  </div>
+
+                  <div className='buttons-container'>
+                    <button className='read-more' onClick={() => readMore()}>
+                      Read more
+                    </button>
+                    <button className='add-favorite' onClick={handleHeart}>
+                      <i
+                        className={`box ${
+                          heart === false ? 'heart' : 'heart heart-click'
+                        }`}
+                      />
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+          )
+        }
       </motion.div>
       {selectedId && (
         <Backdrop onClick={() => setSelectedId(null)}>
--- src/containers/Agentes/AgentesContainer.jsx
+++ src/containers/Agentes/AgentesContainer.jsx
@@ -5,18 +5,19 @@ import './Styles.scss'
 
 export const AgentesContainer = () => {
   const [agentes, setAgentes] = useState([])
+  const [loading, setLoading] = useState(false)
 
   const API_AGENTES = 'https://valorant-api.com/v1/agents?isPlayableCharacter=true'
 
   useEffect(() => {
-    const getAgentes = () => {
-      const response = axios.get(API_AGENTES)
+    const getAgentes = async () => {
+      await axios.get(API_AGENTES)
         .then(res => {
           setAgentes(res.data.data)
         }).catch(err => {
           console.log(err)
         })
-      console.log('response: ', response)
+      setLoading(true)
     }
     getAgentes()
   }, [])
@@ -24,18 +25,23 @@ export const AgentesContainer = () => {
   return (
     <div className='agentes-container'>
       {
-        agentes.map(agente => (
-          <Card
-            key={agente.uuid}
-            agentes={agentes}
-            agente={agente}
-            ability1={agente.abilities[0]}
-            ability2={agente.abilities[1]}
-            ability3={agente.abilities[2]}
-            ability4={agente.abilities[3]}
-
-          />
-        ))
+        loading
+          ? (
+              agentes.map(agente => (
+            <Card
+              key={agente.uuid}
+              agentes={agentes}
+              agente={agente}
+              ability1={agente.abilities[0]}
+              ability2={agente.abilities[1]}
+              ability3={agente.abilities[2]}
+              ability4={agente.abilities[3]}
+            />
+              ))
+            )
+          : (
+            <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+            )
       }
     </div>
   )
